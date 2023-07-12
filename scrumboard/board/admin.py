605d091671f639ca5de477f57631ff9646b72443
MavrinSergey <106872266+MavrinSergey@users.noreply.github.com>
2023-07-12 07:35:02 +0400

    Update admin.py

--- scrumboard/board/admin.py
+++ scrumboard/board/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import User, Task, SignIn, StatusTask, Company, Project, StatusUserProjects, ProjectParticipants
+from .models import User, Task, SignIn, Status
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Task)
 admin.site.register(SignIn)
 admin.site.register(StatusTask)
@@ -9,3 +14,25 @@ admin.site.register(Project)
 admin.site.register(Company)
 admin.site.register(StatusUserProjects)
 admin.site.register(ProjectParticipants)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
