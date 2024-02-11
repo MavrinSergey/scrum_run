b9c94703770a8173a8ded50ba9bdada097138a44
StayAWSM <alex.babichev.95@gmail.com>
2024-02-11 10:11:36 +0300

    Убрали неиспользуемые импорты

--- scrumboard/board/views.py
+++ scrumboard/board/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
